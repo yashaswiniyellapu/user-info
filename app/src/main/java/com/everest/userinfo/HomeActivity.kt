6c08a1adf1d6128e355b2e9de271af883c547313
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-03-01 00:09:09 +0530

    Add cancel button to revert editing

--- app/src/main/java/com/everest/userinfo/HomeActivity.kt
+++ app/src/main/java/com/everest/userinfo/HomeActivity.kt
@@ -13,16 +13,29 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        binding= ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.validateButton.setOnClickListener{
+        binding.validateButton.setOnClickListener {
 
 
-           if( !ValidateFields(binding,this).validateInputs() && !ValidateFields(binding,this).validateMandatoryInputs())
-           {
-               binding.inputTV.visibility=View.GONE
-           }
+            if (!ValidateFields(binding, this).validateInputs() && !ValidateFields(
+                    binding,
+                    this
+                ).validateMandatoryInputs()
+            ) {
+                binding.inputTV.visibility = View.GONE
+                binding.displayTV.visibility= View.VISIBLE
+                binding.disUsernameTV.text = binding.inputUserName.text
+                binding.disEmailTV.text = binding.inputEmail.text
+                binding.disPhoneNumberTV.text = binding.inputPh.text
+                binding.disPinCodeTV.text = binding.inputPinCode.text
+                binding.disAddressTV.text = binding.inputAddress.text
+            }
 
+        }
+        binding.cancelButton.setOnClickListener{
+            binding.inputTV.visibility= View.VISIBLE
+            binding.displayTV.visibility=View.GONE
         }
 
 
