4ef97453a9aa4048d24ad7a5ce08c0ca983de1e3
yashaswiniyellapu <yashaswini.yellapu@everest.engineering>
2022-03-07 11:56:58 +0530

    Add constants file

---
+++ app/src/main/java/com/everest/userinfo/constants/Constants.kt
@@ -0,0 +1,7 @@
+package com.everest.userinfo
+
+const val USER_NAME= "USER_NAME"
+const val PHONE_NUMBER= "PHONE_NUMBER"
+const val ADDRESS= "ADDRESS"
+const val PIN_CODE= "PIN_CODE"
+const val EMAIL= "EMAIL"
